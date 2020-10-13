cbf71031cc7992f05938ca56357d70ba219ba4c8
Adriano P. Almeida <adriano.almeida@inpe.br>
2020-10-12 23:10:03 -0300

    Add scripts to create and populate tables

---
+++ database/queries_insert_games.sql
@@ -0,0 +1,7 @@
+INSERT INTO Game (name, type) VALUES ('Bohnanza', 'card game');
+INSERT INTO Game (name, type) VALUES ('Encantados', 'card game');
+INSERT INTO Game (name, type) VALUES ('Lhama', 'card game');
+INSERT INTO Game (name, type) VALUES ('Munchkin', 'card game');
+INSERT INTO Game (name, type) VALUES ('Ninja Camp', 'card game');
+INSERT INTO Game (name, type) VALUES ('Port Royal', 'card game');
+INSERT INTO Game (name, type) VALUES ('Saboteur', 'card game');
